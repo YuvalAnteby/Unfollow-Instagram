151f8d69a9703833cdbff8effb60f0e5ceb0779c
Yuval <yuvala2001@gmail.com>
2025-09-19 15:41:06 +0300

    clicking on a username opens their profile

--- src/react_front/src/components/ResultViewer.jsx
+++ src/react_front/src/components/ResultViewer.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 /**
  * ResultViewer: displays the list of unfollowers or a message if none.
  */
-export default function ResultViewer({ unfollowers }) {
+export default function ResultViewer({unfollowers}) {
+
+    const usernameClick = (username) => {
+        console.log('click ' + username);
+        window.open(`https://www.instagram.com/${username}`, "_blank");
+    };
+
     return (
-        <div style={{ marginTop: 20 }}>
+        <div style={{marginTop: 20}}>
             <h2>Unfollowers</h2>
             {unfollowers.length === 0 ? (
                 <p>No unfollowers!</p>
             ) : (
                 <ul>
-                    {unfollowers.map(u => <li key={u}>{u}</li>)}
+                    {unfollowers.map(u =>
+                        <li key={u} onClick={() => usernameClick(u)}
+                        >
+                            {u}
+                        </li>
+                    )}
                 </ul>
             )}
         </div>
